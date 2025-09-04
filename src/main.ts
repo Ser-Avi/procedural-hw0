f482b488fbb59bf59762d3bee235a4a111574867
Ser-Avi <128562182+Ser-Avi@users.noreply.github.com>
2025-09-04 18:57:13 -0400

    Parts 1 through 4. Added color picker

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ const Stats = require('stats-js');
 import * as DAT from 'dat.gui';
 import Icosphere from './geometry/Icosphere';
 import Square from './geometry/Square';
+import Cube from './rendering/gl/Cube';
 import OpenGLRenderer from './rendering/gl/OpenGLRenderer';
 import Camera from './Camera';
 import {setGL} from './globals';
@@ -13,10 +14,12 @@ import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
 const controls = {
   tesselations: 5,
   'Load Scene': loadScene, // A function pointer, essentially
+  color : [0, 0, 1, 255],
 };
 
 let icosphere: Icosphere;
 let square: Square;
+let cube: Cube;
 let prevTesselations: number = 5;
 
 function loadScene() {
@@ -24,6 +27,8 @@ function loadScene() {
   icosphere.create();
   square = new Square(vec3.fromValues(0, 0, 0));
   square.create();
+  cube = new Cube(vec3.fromValues(1, 0, 0));
+  cube.create();
 }
 
 function main() {
@@ -39,6 +44,7 @@ function main() {
   const gui = new DAT.GUI();
   gui.add(controls, 'tesselations', 0, 8).step(1);
   gui.add(controls, 'Load Scene');
+  gui.addColor(controls, 'color');
 
   // get canvas and webgl context
   const canvas = <HTMLCanvasElement> document.getElementById('canvas');
@@ -73,13 +79,16 @@ function main() {
     if(controls.tesselations != prevTesselations)
     {
       prevTesselations = controls.tesselations;
-      icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, prevTesselations);
-      icosphere.create();
+      // icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, prevTesselations);
+      // icosphere.create();
+      cube = new Cube(vec3.fromValues(1, 0, 0));
+      cube.create();
     }
     renderer.render(camera, lambert, [
-      icosphere,
+      //icosphere,
       // square,
-    ]);
+      cube,
+    ], controls.color.map(value => value / 255));
     stats.end();
 
     // Tell the browser to call `tick` again whenever it renders a new frame
---
+++ src/rendering/gl/Cube.ts
@@ -0,0 +1,122 @@
+import Drawable from '../../rendering/gl/Drawable'
+import {gl} from '../../globals';
+import {vec3, vec4} from 'gl-matrix';
+
+class Cube extends Drawable {
+    indices: Uint32Array;
+    positions: Float32Array;
+    normals: Float32Array;
+    center: vec4;
+
+    constructor(center: vec3) {
+        super();
+        this.center = vec4.fromValues(center[0], center[1], center[2], 1);
+    }
+
+    create() {
+        this.indices = new Uint32Array([
+                            // front
+                            0, 1, 2,
+                            0, 2, 3,
+                            // back
+                            4, 5, 6,
+                            4, 6, 7,
+                            // top
+                            8, 9, 10,
+                            8, 10, 11,
+                            // bot
+                            12, 13, 14,
+                            12, 14, 15,
+                            // R
+                            16, 17, 18,
+                            16, 18, 19,
+                            // L
+                            20, 21, 22,
+                            20, 22, 23
+                        ]);
+
+        this.normals = new Float32Array([
+                            // front
+                            0, 0, 1, 0,
+                            0, 0, 1, 0,
+                            0, 0, 1, 0,
+                            0, 0, 1, 0,
+                            // back
+                            0, 0, -1, 0,
+                            0, 0, -1, 0,
+                            0, 0, -1, 0,
+                            0, 0, -1, 0,
+                            // top
+                            0, 1, 0, 0,
+                            0, 1, 0, 0,
+                            0, 1, 0, 0,
+                            0, 1, 0, 0,
+                            // bot
+                            0, -1, 0, 0,
+                            0, -1, 0, 0,
+                            0, -1, 0, 0,
+                            0, -1, 0, 0,
+                            // R
+                            1, 0, 0, 0,
+                            1, 0, 0, 0,
+                            1, 0, 0, 0,
+                            1, 0, 0, 0,
+                            // L
+                            -1, 0, 0, 0,
+                            -1, 0, 0, 0,
+                            -1, 0, 0, 0,
+                            -1, 0, 0, 0
+                        ]);
+
+        this.positions = new Float32Array([
+                            // front
+                            -1, -1, 1, 1,
+                            1, -1, 1, 1,
+                            1,  1, 1, 1,
+                            -1,  1, 1, 1,
+                            // back
+                            -1, -1, -1, 1,
+                            1, -1, -1, 1,
+                            1,  1, -1, 1,
+                            -1,  1, -1, 1,
+                            // top
+                            -1, 1, -1, 1,
+                            1, 1, -1, 1,
+                            1, 1,  1, 1,
+                            -1, 1,  1, 1,
+                            // bot
+                            -1, -1, -1, 1,
+                            1, -1, -1, 1,
+                            1, -1,  1, 1,
+                            -1, -1,  1, 1,
+                            // R
+                            1, -1, -1, 1,
+                            1,  1, -1, 1,
+                            1,  1,  1, 1,
+                            1, -1,  1, 1,
+                            // L
+                            -1, -1, -1, 1,
+                            -1,  1, -1, 1,
+                            -1,  1,  1, 1,
+                            -1, -1,  1, 1
+                        ]);
+
+        this.generateIdx();
+        this.generatePos();
+    this.generateNor();
+
+    this.count = this.indices.length;
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufIdx);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.bufNor);
+    gl.bufferData(gl.ARRAY_BUFFER, this.normals, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.bufPos);
+    gl.bufferData(gl.ARRAY_BUFFER, this.positions, gl.STATIC_DRAW);
+
+    console.log(`Created cube`);
+    }
+};
+
+export default Cube;    // Note: What is this for?
